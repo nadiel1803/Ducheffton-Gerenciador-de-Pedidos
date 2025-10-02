91d49d52a61da546dd1a3db715cf05283afd634e
nadiel1803 <danialmeida1803@gmail.com>
2025-10-02 17:14:05 -0300

    correção

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase
 
 /* Sua configuração — mantenha os valores que você já tinha */
 const firebaseConfig = {
-  apiKey: "AIzaSyB2pPpPVRmcKC8D4clCL73PSyuwVUN8wpY",
-  authDomain: "ducheffton-pedidos.firebaseapp.com",
-  projectId: "ducheffton-pedidos",
-  storageBucket: "ducheffton-pedidos.firebasestorage.app",
-  messagingSenderId: "2379935099",
-  appId: "1:2379935099:web:e5131fb3c7beeb9d59a1de"
+  apiKey: "AIzaSyChkboBPp21hQ_tRFQrV2Hi3iBF-WHxWSE",
+  authDomain: "gerenciador-de-pedidos-4ac32.firebaseapp.com",
+  projectId: "gerenciador-de-pedidos-4ac32",
+  storageBucket: "gerenciador-de-pedidos-4ac32.firebasestorage.app",
+  messagingSenderId: "674975621927",
+  appId: "1:674975621927:web:a09ad1f5c57c2dcb00ff02",
+  measurementId: "G-FWSS1YTR2J"
 };
 
 const app = initializeApp(firebaseConfig);
